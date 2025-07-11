3411936fdf6593cd76730414faada3aa9eba230f
Evelyn Salie <ekobaddish@gmail.com>
2025-07-11 01:12:44 -0400

    Added ability to tag parsed markdown

---
+++ MarkDown.js
@@ -0,0 +1,218 @@
+class MarkDown {
+	// Parse the passed string as markdown.
+	// This accepts the following tokens:
+	// Bold Delimiter: **
+	// Italics Delimiter: *
+	// Code Delimiter: `
+	// Think of a delimiter as toggling a flag which denotes the presence of its associated effect.
+	constructor(str, token_map) {
+		if (!(token_map instanceof Map))
+			throw new Error("token_map must be Map")
+		
+		for (let token of token_map.keys()) {
+			if (token.length == 0)
+				throw new Error("Tokens must not be empty string.")
+		}
+		
+		console.log("Attempting to parse '" + str + "'")
+		
+		// An array of pairs. Each of a string and a Set of tags which apply to that string.
+		this.segments = []
+		
+		let tok_data = MarkDown.get_next_token(str, token_map)
+		let current_tags = new Set()
+		let num_iters = 0
+		while (tok_data[0] != -1) {
+			num_iters++
+			if (num_iters > 100)
+				throw new Error("Iter limit reached.")
+			
+			console.assert(tok_data[1] != null)
+			
+			let token_i = tok_data[0]
+			let token = tok_data[1]
+			let tag = token_map.get(token)
+			
+//			console.log("Token '" + token + "' at " + tag + ".")
+			
+			if (token_i > 0) {
+				// Push a portion of the string and the current tag list to the segments array.
+				this.segments.push([
+					str.slice(0, token_i),
+					new Set(current_tags)
+				])
+			}
+			
+			// Update the string and tag set
+			str = str.slice(token_i + token.length)
+			if (current_tags.has(tag)) current_tags.delete(tag)
+			else current_tags.add(tag)
+			
+			tok_data = MarkDown.get_next_token(str, token_map)
+		}
+		
+		// Technically it makes no sense to push these tags since there are no
+		// remaining tokens and thus, all tags open are unclosed and will be removed in the next step.
+		// However, since this push might not even occur if the str is length 0, these tags must
+		// be pushed to allow the removal step proceed correctly.
+		if (str.length > 0) {
+			this.segments.push([
+				str,
+				new Set(current_tags)
+			])
+		}
+		
+		// Any active tags are unclosed and should be retroactively removed.
+		for (let segment_i = this.segments.length - 1; segment_i >= 0; segment_i--) {
+			let segment = this.segments[segment_i]
+			
+			for (let tag of current_tags) {
+				if (segment[1].has(tag)) {
+					segment[1].delete(tag)
+				}
+				else {
+					current_tags.delete(tag)
+				}
+			}
+		}
+		
+		console.log("Parsing Complete")
+	}
+	
+	// Apply a tag to the characters in the specified range.
+	add_tag(tag, start, end) {
+		if (end <= start)
+			throw new Error("start index must be less than end index.")
+		
+		if (end < 0)
+			throw new Error("end index must be positive")
+		
+		if (start < 0)
+			throw new Error("start index must be positive")
+		
+		for (let i = 0; i < this.segments.length; i++) {
+			let segment = this.segments[i]
+			console.log(i + ": " + "'" + segment[0] + "', " + start + ", " + end)
+			
+			// This segment contains the start.
+			if (start > 0 && start < segment[0].length) {
+				console.log("Contains Start...")
+				// Split the text
+				let left_str = this.segments[i][0].slice(0, start)
+				let right_str = this.segments[i][0].slice(start)
+				
+				// Split this segment in two and tag the right half.
+				
+				this.segments.splice(i+1, 0, [
+					right_str,
+					new Set(this.segments[i][1]) // Copy the tags
+				])
+				
+				this.segments[i][0] = left_str
+			}
+			// This segment is past the start.
+			else if (start <= 0) {
+				console.log("Past Start...")
+				// This segment is before the end
+				if (end >= segment[0].length) {
+					console.log("Before End...")
+					this.segments[i][1].add(tag)
+				}
+				// This segment contains the end
+				else if (end > 0 && end < segment[0].length) {
+					console.log("Contains End...")
+					// Split the text
+					let left_str = this.segments[i][0].slice(0, end)
+					let right_str = this.segments[i][0].slice(end)
+					
+					// Split this segment in two and tag the left half.
+					
+					this.segments.splice(i+1, 0, [
+						right_str,
+						new Set(this.segments[i][1]) // Copy the tags
+					])
+					
+					this.segments[i][0] = left_str
+					this.segments[i][1].add(tag)
+					
+					break
+				}
+				// This segment is past the end
+				else {
+					console.log("Past End...")
+					break
+				}
+			}
+			
+			// Track the relative start and end.
+			start -= this.segments[i][0].length
+			end -= this.segments[i][0].length
+		}
+	}
+	
+	debug_segments() {
+		for (let segment of this.segments) {
+			let str = segment[0]
+			let tags = segment[1]
+			
+			let debug_str = "'" + str + "': "
+			for (let key of tags) debug_str += key + ", "
+			
+			console.log(debug_str)
+		}
+	}
+	
+	// Retrieve the first token in the string.
+	// Returns the index of the first character in the token and the token itself in an array.
+	// Returns [-1, null] if no token was found.
+	static get_next_token(str, token_map) {
+		let first_token = null
+		let first_token_i = -1
+		
+		let cursor = 0
+		let num_iters = 0
+		while (true) {
+			num_iters++
+			if (num_iters > 100)
+				throw new Error("Iter limit reached.")
+			
+			// Get the first token.
+			for (let token of token_map.keys()) {
+				console.assert(token.length > 0)
+				
+				let i = str.indexOf(token, cursor)
+				if (i != -1) {
+					if (first_token == null || i < first_token_i) {
+						first_token = token
+						first_token_i = i
+					}
+				}
+			}
+			
+			// Check that the token is not escaped.
+			// This arrangement ensures that a construct such as \** is interpreted as
+			// an escaped bold token rather than escaped and unescaped pair of italics tokens
+			if (first_token_i > 0 && str[first_token_i - 1] == "\\") {
+				cursor = first_token_i + first_token.length
+				
+				first_token = null
+				first_token_i = -1
+			}
+			else {
+				break
+			}
+		}
+		
+		return [first_token_i, first_token]
+	}
+}
+
+let token_map = new Map([["**", "b"], ["*", "i"]])
+
+let md = new MarkDown("none**bold*bolditalic**italic*trail", token_map)
+// new MarkDown("**unclosedbold*uncloseditalic", token_map).debug_segments()
+// new MarkDown("**unclosedbold*closeditalic*trail", token_map).debug_segments()
+// new MarkDown("**closedbold**none**unclosedbold*closeditalic*", token_map).debug_segments()
+// new MarkDown("**bold\\**bold**none*italics\\*italics*", token_map).debug_segments()
+md.add_tag("u", 3, 9)
+md.debug_segments()
